9ff507b27d6a6200cb735e9effbab3d39626ea89
DianaAnisutina <freilina_07@mail.ru>
2013-04-24 19:43:09 +0700

    1 stage of morphing
    the image can be morphed
    top half
    the layer filter
    nearest type

--- Morph/FIT0201ANISUTINA_Morph/mainwindow.cpp
+++ Morph/FIT0201ANISUTINA_Morph/mainwindow.cpp
@@ -8,12 +8,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
     scene = QImage(600, 400, QImage::Format_RGB888);
-    redrawScene();
     baseTexture = QImage(":/baseTexture.png");
     texel = QSize(4, 4);
-
-    fillMatrix(0, 256);
-    gausSolver();
+    initializePoints();
+    redrawScene();
 }
 
 MainWindow::~MainWindow() {
@@ -26,29 +24,13 @@ void MainWindow::paintEvent(QPaintEvent *) {
 }
 
 void MainWindow::resizeEvent(QResizeEvent *e) {
-    scene = QImage(e->size().width() - 90, e->size().height() - 40, QImage::Format_RGB888);
-    clearScene();
-    redrawScene();
+    //scene = QImage(e->size().width() - 90, e->size().height() - 40, QImage::Format_RGB888);
+   // clearScene();
+   // redrawScene();
 }
 
 void MainWindow::clearScene() {
     scene.fill(QColor(255, 255, 255).rgba());
-    QSize size = scene.size();
-    a_point = QPoint(size.width() / 2 - 128, size.height() / 2 - 128);
-    b_point = QPoint(size.width() / 2 + 128, size.height() / 2 - 128);
-    c_point = QPoint(size.width() / 2 + 128, size.height() / 2);
-    f_point = QPoint(size.width() / 2 - 128, size.height() / 2);
-    e_point = QPoint(size.width() / 2 - 128, size.height() / 2 + 128);
-    d_point = QPoint(size.width() / 2 + 128, size.height() / 2 + 128);
-    c_point_current = c_point;
-    f_point_current = f_point;
-
-    setPoint(a_point);
-    setPoint(b_point);
-    setPoint(c_point_current);
-    setPoint(d_point);
-    setPoint(e_point);
-    setPoint(f_point_current);
     update();
 }
 
@@ -90,25 +72,17 @@ void MainWindow::on_fPointSlider_sliderMoved(int position) {
 }
 
 void MainWindow::redrawScene() {
-    scene.fill(QColor(255, 255, 255).rgba());
-    setPoint(a_point);
-    setPoint(b_point);
-    setPoint(c_point_current);
-    setPoint(d_point);
-    setPoint(e_point);
-    setPoint(f_point_current);
-    drawLine(a_point, b_point, QColor(0, 0, 0).rgba());
-    drawLine(b_point, c_point_current, QColor(0, 0, 0).rgba());
-    drawLine(c_point_current, d_point, QColor(0, 0, 0).rgba());
-    drawLine(d_point, e_point, QColor(0, 0, 0).rgba());
-    drawLine(e_point, f_point_current, QColor(0, 0, 0).rgba());
-    drawLine(f_point_current, a_point, QColor(0, 0, 0).rgba());
-    renderingTexturedImage();
+    scene.fill(QColor(255, 255, 255).rgba()); //clearScene
+    drawBorders();
+    QVector<qreal> coefficientsForTopTransform = gausSolver(fillTopMatrix(f_point_current, c_point_current));
+    //QVector<qreal> coefficientsBottomTopTransform = gausSolver(fillBottomMatrix(relativeCoordinates(f_point), relativeCoordinates(c_point)));
+    renderingTexturedImage(coefficientsForTopTransform);//, coefficientsForTopTransform);
     update();
 }
 
 void MainWindow::on_cPointSlider_sliderMoved(int position) {
     c_point_current = QPoint(c_point.x() + position, c_point.y());
+
     redrawScene();
 }
 
@@ -129,7 +103,7 @@ void MainWindow::drawLine(QPoint pnt1, QPoint pnt2, QRgb clr) {
     int err = deltaX - deltaY;
     setPixelSafe(end_x, end_y, clr);
     while((end_x != curr_x) || (end_y != curr_y)) {
-        setPixelSafe(curr_x, curr_y, clr);
+        setPixelSafe(curr_x, curr_y, clr);//append to vector of points
         int err2 = err * 2;
         if(err2 > -deltaY) {
             err -= deltaY;
@@ -158,15 +132,30 @@ void MainWindow::drawVLine(QPoint pnt1, QPoint pnt2, QRgb clr) {
     }
 }
 
-void MainWindow::renderingTexturedImage() {
-    //bypass every inner point
+void MainWindow::renderingTexturedImage(QVector<qreal> topCoeffs) {
+
+    //bypass every inner point using brsenhem
+    //get new 4 points
+    //calc
     //setPixel(i, j, layerNearestFilter(i, j))
-    for(int i = a_point.x(); i < b_point.x(); i++) {
-        for(int j = a_point.y(); j < d_point.y(); j++) {
-            QRgb color = layerNearestFilter(i - a_point.x(), j - a_point.y());
-            setPixelSafe(i, j, color);
+    // get points from brezenhem
+    QVector<QPoint> af_line;
+    QVector<QPoint> bc_line;
+    af_line = getPointsOfBrezenhemLine(toSceneCoordinates(a_point), toSceneCoordinates(f_point_current), af_line);
+    bc_line = getPointsOfBrezenhemLine(toSceneCoordinates(b_point), toSceneCoordinates(c_point_current), bc_line);
+    for(int h = 0; h < 128; h++) {
+        for(int w = af_line[h].x(); w < bc_line[h].x(); w++) {
+            QRgb color = layerNearestFilter(getTexturePoint(QPoint(w, h), topCoeffs));
+            setPixelSafe(toSceneCoordinates(QPoint(w, h)), color);
         }
     }
+
+ /*   for(int i = a_point.x(); i < b_point.x(); i++) {
+        for(int j = a_point.y(); j < d_point.y(); j++) {
+            QRgb color = layerNearestFilter(getTexturePoint( i - a_point.x(), j - a_point.y());
+            setPixelSafe(i, j, color);
+        }
+    }*/
 /*
     for(int i = a_point.x(); i < b_point.x(); i++) {
         for(int j = a_point.y(); j < d_point.y(); j++) {
@@ -178,12 +167,16 @@ void MainWindow::renderingTexturedImage() {
 }
 
 QRgb MainWindow::layerNearestFilter(int horz_coord, int vert_coord) {
-    int x_coord = horz_coord * texel.width() + texel.width() / 2;
-    int y_coord = vert_coord * texel.height() + texel.height() / 2;
+    int x_coord = (horz_coord / texel.width()) * texel.width() + texel.width() / 2;
+    int y_coord = (vert_coord / texel.height()) * texel.height() + texel.height() / 2;
     if (pixelSafe(x_coord, y_coord, baseTexture.width(), baseTexture.height())) {
         return baseTexture.pixel(x_coord, y_coord);
     }
-   return 0;//QRgb(0, 0, 0); // is it ok?
+   return 0;//QRgb(0, 0, 0); // is it ok? <- mirroring
+}
+
+QRgb MainWindow::layerNearestFilter(QPoint pnt) {
+    return layerNearestFilter(pnt.x(), pnt.y());
 }
 
 QRgb MainWindow::layerLinearFilter(int horz_coord, int vert_coord) {
@@ -203,12 +196,13 @@ QRgb MainWindow::layerLinearFilter(int horz_coord, int vert_coord) {
     return baseTexture.pixel(horz_coord / texel.width() + texel.width() / 2, vert_coord / texel.height() + texel.height() / 2);
 }
 
-void MainWindow::gausSolver() {
+QVector<qreal> MainWindow::gausSolver(QVector<QVector<qreal> > slae) {
 
+    QVector<qreal> coeffTransform;
     //1st pass to make upper triangular matrix
     for(int i = 0; i < slae.size(); i++) {
         if(0 == slae[i][i]) {
-            for(int j = i + 1 ; j < slae.size(); j++) { //i+1
+            for(int j = i + 1 ; j < slae.size(); j++) {
                 if(slae[j][i] != 0) {
                     slae[i].swap(slae[j]);
                     break;
@@ -238,14 +232,17 @@ void MainWindow::gausSolver() {
             slae[j][i] = 0.0;
         }
     }
+
     for(int i = 0; i < slae.size(); i++) {
-        qDebug() << slae[i][slae[i].size() - 1];
+        coeffTransform.append(slae[i][slae[i].size() - 1]);
     }
-    return;
+    qDebug() << coeffTransform;
+    return coeffTransform;
 }
 
-void MainWindow::fillMatrix(int x1_star, int x2_star) {
+QVector<QVector<qreal> > MainWindow::fillTopMatrix(int x1_star, int x2_star) {
 
+    QVector<QVector<qreal> > slae;
     QVector<qreal> rowMatrix = QVector<qreal>(9, 0.0);
     rowMatrix.replace(2, 1.0);
     slae.append(rowMatrix);
@@ -298,4 +295,85 @@ void MainWindow::fillMatrix(int x1_star, int x2_star) {
     rowMatrix.replace(7, -128.0);
     rowMatrix.replace(8, 1.0);
     slae.append(rowMatrix);
+    return slae;
+}
+
+QVector<QVector<qreal> > MainWindow::fillTopMatrix(QPoint f_pnt, QPoint c_pnt) {
+    return fillTopMatrix(f_pnt.x(), c_pnt.x());
+}
+
+QPoint MainWindow::relativeCoordinates(QPoint pnt) {
+    return QPoint(pnt.x() - (scene.width() / 2 - 128), pnt.y() - (scene.height() / 2 - 128));
+}
+
+void MainWindow::initializePoints() {
+    a_point = QPoint(0, 0);
+    b_point = QPoint(256, 0);
+    c_point = QPoint(256, 128);
+    f_point = QPoint(0, 128);
+    e_point = QPoint(0, 256);
+    d_point = QPoint(256, 256);
+    c_point_current = c_point;
+    f_point_current = f_point;
+}
+
+void MainWindow::drawBorders() {
+    setPoint(toSceneCoordinates(a_point));
+    setPoint(toSceneCoordinates(b_point));
+    setPoint(toSceneCoordinates(c_point_current));
+    setPoint(toSceneCoordinates(d_point));
+    setPoint(toSceneCoordinates(e_point));
+    setPoint(toSceneCoordinates(f_point_current));
+    drawLine(toSceneCoordinates(a_point), toSceneCoordinates(b_point), QColor(0, 0, 0).rgba());
+    drawLine(toSceneCoordinates(b_point), toSceneCoordinates(c_point_current), QColor(0, 0, 0).rgba());
+    drawLine(toSceneCoordinates(c_point_current), toSceneCoordinates(d_point), QColor(0, 0, 0).rgba());
+    drawLine(toSceneCoordinates(d_point), toSceneCoordinates(e_point), QColor(0, 0, 0).rgba());
+    drawLine(toSceneCoordinates(e_point), toSceneCoordinates(f_point_current), QColor(0, 0, 0).rgba());
+    drawLine(toSceneCoordinates(f_point_current), toSceneCoordinates(a_point), QColor(0, 0, 0).rgba());
+}
+
+QPoint MainWindow::getTexturePoint(QPoint pnt, QVector<qreal> coeffs) {
+    qreal u_component = (pnt.x() * coeffs[0] + pnt.y() * coeffs[1] + coeffs[2]) / (pnt.x() * coeffs[6] + pnt.y() * coeffs[7] + 1);
+    qreal v_component = (pnt.x() * coeffs[3] + pnt.y() * coeffs[4] + coeffs[5]) / (pnt.x() * coeffs[6] + pnt.y() * coeffs[7] + 1);
+    if (1.0 < u_component) {
+        u_component -= 1.0;
+    }
+    if (1.0 < v_component) {
+        v_component -= 1.0;
+    }
+    if (0.0 > u_component) {
+        u_component += 1.0;
+    }
+    if (0.0 > v_component) {
+        v_component += 1.0;
+    }
+    return QPoint(static_cast<int>(u_component * baseTexture.width() - 0.5), static_cast<int>(v_component * baseTexture.height() - 0.5));
+}
+
+QPoint MainWindow::toSceneCoordinates(QPoint pnt) {
+    return QPoint((scene.width() / 2 - 128) + pnt.x(), (scene.height() / 2 - 128) + pnt.y());
+}
+
+QVector<QPoint> MainWindow::getPointsOfBrezenhemLine(QPoint pnt1, QPoint pnt2, QVector<QPoint> line) {
+    int curr_x = pnt1.x(), curr_y = pnt1.y(), end_x = pnt2.x(), end_y = pnt2.y();
+
+    int deltaX = qAbs(end_x - curr_x);
+    int deltaY = qAbs(end_y - curr_y);
+    int signX = curr_x < end_x ? 1 : -1;
+    int signY = curr_y < end_y ? 1 : -1;
+    int err = deltaX - deltaY;
+    while((end_x != curr_x) || (end_y != curr_y)) {
+        line.append(relativeCoordinates(QPoint(curr_x, curr_y)));//append to vector of points
+        int err2 = err * 2;
+        if(err2 > -deltaY) {
+            err -= deltaY;
+            curr_x += signX;
+        }
+        if(err2 < deltaX) {
+            err += deltaX;
+            curr_y += signY;
+        }
+    }
+    line.append(relativeCoordinates(QPoint(end_x, end_y)));
+    return line;
 }
--- Morph/FIT0201ANISUTINA_Morph/mainwindow.h
+++ Morph/FIT0201ANISUTINA_Morph/mainwindow.h
@@ -27,11 +27,22 @@ public:
     void drawLine(QPoint, QPoint, QRgb);
     void drawHLine(QPoint, QPoint, QRgb);
     void drawVLine(QPoint, QPoint, QRgb);
-    void renderingTexturedImage();
+    void renderingTexturedImage(QVector<qreal> topCoeffs);//, QVector<qreal> bottomCoeffs);
     QRgb layerNearestFilter(int, int);
+    QRgb layerNearestFilter(QPoint);
     QRgb layerLinearFilter(int, int);
-    void gausSolver();
-    void fillMatrix(int x1_star, int x2_star);
+    //void gausSolver(QVector<QVector<qreal> > slae);
+    QVector<qreal> gausSolver(QVector<QVector<qreal> > slae);
+    QVector<QVector<qreal> > fillTopMatrix(int x1_star, int x2_star);
+    QVector<QVector<qreal> > fillTopMatrix(QPoint f_pnt, QPoint c_pnt);
+    QVector<QVector<qreal> > fillBottomMatrix(int x1_star, int x2_star);
+    QVector<QVector<qreal> > fillBottomMatrix(QPoint f_pnt, QPoint c_pnt);
+    QPoint relativeCoordinates(QPoint pnt);
+    void initializePoints();
+    void drawBorders();
+    QPoint getTexturePoint(QPoint pnt, QVector<qreal> coeffs);
+    QPoint toSceneCoordinates(QPoint);
+    QVector<QPoint> getPointsOfBrezenhemLine(QPoint start_line, QPoint end_line, QVector<QPoint> line);
 
 private slots:
     void on_fPointSlider_sliderMoved(int position);
@@ -50,8 +61,8 @@ private:
     QPoint e_point;
     QImage baseTexture;
     QSize texel;
-    QMatrix coeffTransform;
-    QVector<QVector<qreal> > slae;
+    //QVector<qreal> coeffTransform;
+    //QVector<QVector<qreal> > slae;
 };
 
 #endif // MAINWINDOW_H
